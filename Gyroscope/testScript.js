db178fddc30a230204e67814eecfe20e03cb2cf1
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2022-06-18 18:59:16 +0530

    he

--- Gyroscope/testScript.js
+++ Gyroscope/testScript.js
@@ -9,6 +9,6 @@ motionHandler.deviceMotion.interval = 30;
 //console.log(motionHandler.deviceMotion);
 setInterval(handle, 1000);
 function handle() {
-  x.innerText = JSON.stringify(motionHandler.deviceMotion);
+  x.innerText = JSON.stringify(motionHandler);
   console.log(motionHandler.deviceMotion);
 }
