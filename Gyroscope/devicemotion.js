336dc21edea88852e8a5956d0e7f8f6dd0f7964c
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2022-06-18 20:06:43 +0530

    final update

--- Gyroscope/devicemotion.js
+++ Gyroscope/devicemotion.js
@@ -1,17 +1,63 @@
 class DeviceMotion {
   timer = {
     T: 1000,
-    timer: null,
+    timer: undefined,
     setT() {
       this.timer = true;
       setTimeout(() => {
         this.timer = false;
-      }, this.T || 1000);
+      }, this.T);
     },
   };
+
   deviceMotion;
-  constructor(alphaFn, betaFn, gammaFn, reactTime = 60) {
-    this.deviceMotion = new DeviceMotionEvent("devicemotion");
+  event;
+  x;
+  y;
+  z;
+  ax;
+  ay;
+  az;
+  alphaFn;
+  betaFn;
+  gammaFn;
+
+  constructor(
+    alphaFn = null,
+    betaFn = null,
+    gammaFn = null,
+    reactTime = 1000,
+    ax = 15,
+    ay = 15,
+    az = 15
+  ) {
+    this.timer.T = reactTime;
+    [this.ax, this.ay, this.az] = [ax, ay, az];
+    [this.alphaFn, this.betaFn, this.gammaFn] = [alphaFn, betaFn, gammaFn];
+    this.event = window.addEventListener(
+      "devicemotion",
+      (e) => {
+        if (!this.timer.timer) {
+          this.eventFn(e);
+        }
+      },
+      false
+    );
+  }
+  eventFn(e) {
+    this.x = e.acceleration.x;
+    this.y = e.acceleration.y;
+    this.z = e.acceleration.z;
+    if (Math.abs(this.z) > this.az && this.gammaFn) {
+      this.gammaFn();
+      this.timer.setT();
+    } else if (Math.abs(this.x) > this.ax && this.alphaFn) {
+      this.alphaFn();
+      this.timer.setT();
+    } else if (Math.abs(this.y) > this.ay && this.betaFn) {
+      this.betaFn();
+      this.timer.setT();
+    }
   }
 }
 
--- Gyroscope/testScript.js
+++ Gyroscope/testScript.js
@@ -4,11 +4,19 @@ const y = document.getElementById("y");
 const z = document.getElementById("z");
 const lable = document.getElementById("lable");
 
-const motionHandler = new DeviceMotion();
-
+const motionHandler = new DeviceMotion(alphafn, betafn, gammafn, 500);
+setInterval(() => {
+  x.innerText = motionHandler.x;
+  y.innerText = motionHandler.y;
+  z.innerText = motionHandler.z;
+}, 500);
 //console.log(motionHandler.deviceMotion);
-setInterval(handle, 1000);
-function handle() {
-  x.innerText = JSON.stringify(motionHandler.deviceMotion.acceleration);
-  console.log(motionHandler.deviceMotion);
+function alphafn() {
+  alert("swipe Right/left");
+}
+function betafn() {
+  alert("rotate");
+}
+function gammafn() {
+  alert("drop");
 }
